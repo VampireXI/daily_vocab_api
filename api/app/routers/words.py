44df3bd75ac1b8e75da08eb52d0484d63c239d16
VampireXI <gapmanutsawin@gmail.com>
2025-11-08 13:22:01 +0700

    test

--- api/app/routers/words.py
+++ api/app/routers/words.py
@@ -3,7 +3,8 @@ import random
 
 from app.models import Word
 from app.schemas import WordResponse
-
+from sqlalchemy.orm import Session
+from app.database import get_db
 router = APIRouter()
 
 
@@ -23,6 +24,12 @@ words = [
 
 
 @router.get("/word", response_model=WordResponse)
-def get_random_word():
-    random_word = random.choice(words)
-    return random_word
+def get_random_word(db: Session = Depends(get_db)):
+        words = db.query(Word).all()
+        if not words:
+                raise HTTPException(
+                status_code=404,
+                detail='No words available in database'
+                )
+        random_word = random.choice(words)
+        return random_word
